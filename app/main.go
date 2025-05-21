4d49598f3e9893b86e72bedcaa1b532633b40442
laborant <laborant@labs.iximiuz.com>
2025-05-21 07:28:41 +0000

    added tests

--- app/main.go
+++ app/main.go
@@ -14,6 +14,10 @@ type Simple struct {
     
 }
 
+func SimpleFactory(host string) Simple {
+    return Simple{"Hello", "World", host}
+}
+
 func handler(w http.ResponseWriter, r *http.Request) {
     simple := Simple{"Hello", "World", r.Host}
 
